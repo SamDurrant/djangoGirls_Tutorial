34da97c9c4ab78ee1afb353d4af7816d2455684a
Samantha Durrant <samantha.durrant.87@gmail.com>
2020-11-19 13:51:42 -0700

    add drafts and publish functionality. add delete method

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,17 @@ from .forms import PostForm
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
+    return render(request, 'blog/post_draft_list.html', { 'posts': posts })
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
@@ -16,7 +27,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False) # save form but don't save Post model yet
             post.author = request.user # add author to Post
-            post.published_date = timezone.now() # add published_date to post
+            post.created_date = timezone.now() # add created date
             post.save() # save Post model
             return redirect('post_detail', pk=post.pk) # redirect user to new post page
     else:
@@ -29,7 +40,7 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            post.created_date = timezone.now() # add created date
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
