47c847a5e080f0e20643887cc4c03e0fba2b9e6c
Samantha Durrant <samantha.durrant.87@gmail.com>
2020-11-19 12:45:41 -0700

    small bug. things work now

--- blog/views.py
+++ blog/views.py
@@ -27,7 +27,7 @@ def post_edit(request, pk):
     if request.method == 'POST':
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
-            post = form.save(commit=false)
+            post = form.save(commit=False)
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
